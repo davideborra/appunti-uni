bbed3a6f1a56986f218393c98e1962b71de679cc
dborra03 <davide.borra03@gmail.com>
2023-01-05 22:02:38 +0100

    Licenza CC BY NC ND

---
+++ Analisi-A/SPI/script.js
@@ -0,0 +1,36 @@
+var level = 1;
+
+Vue.component("modal", {
+    template: "#modal-template"
+  });
+
+var app = new Vue({
+    el: "#vueContainer",
+    data: {
+        menuIsVisible: false,
+        showModal: false,
+        menuItems: [
+            {
+                name: "",
+                pageSrc: ""
+            }
+        ],
+    },
+    mounted(){
+        checkCookie();
+        setDark(dark, level);
+        this.menuItems=loadLinks(level);
+    },
+    methods: {
+        showMenu(){
+            this.menuIsVisible = !this.menuIsVisible;
+        },
+        changeTheme(){
+            if(dark){
+                setDark(false, level);
+            }else{
+                setDark(true, level);
+            }
+        }       
+    }
+});
